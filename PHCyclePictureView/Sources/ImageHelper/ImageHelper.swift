eb8799d09850ab972680dd7ff9b637ab2dfd1c48
Master <Master@sinois-mbp.lan>
2017-04-11 09:20:01 +0800

    ImageHelper Swift 3.1 Support.

--- PHCyclePictureView/Sources/ImageHelper/ImageHelper.swift
+++ PHCyclePictureView/Sources/ImageHelper/ImageHelper.swift
@@ -577,8 +577,8 @@ public extension UIImage {
         }
         let imageRect = CGRect(origin: CGPoint.zero, size: size)
         var effectImage = self
-        let hasBlur = blurRadius > CGFloat(FLT_EPSILON)
-        let hasSaturationChange = fabs(saturationDeltaFactor - 1.0) > CGFloat(FLT_EPSILON)
+        let hasBlur = blurRadius > CGFloat(Float.ulpOfOne)
+        let hasSaturationChange = fabs(saturationDeltaFactor - 1.0) > CGFloat(Float.ulpOfOne)
         if (hasBlur || hasSaturationChange) {
             
             UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
@@ -604,7 +604,7 @@ public extension UIImage {
             
             if hasBlur {
                 let inputRadius = blurRadius * UIScreen.main.scale
-                let sqrtPi: CGFloat = CGFloat(sqrt(M_PI * 2.0))
+                let sqrtPi: CGFloat = CGFloat(sqrt(Double.pi * 2.0))
                 var radius = UInt32(floor(inputRadius * 3.0 * sqrtPi / 4.0 + 0.5))
                 if radius % 2 != 1 {
                     radius += 1 // force radius to be odd so that the three box-blur methodology works.

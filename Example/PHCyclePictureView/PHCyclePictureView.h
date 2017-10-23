05d84ac04efd5313faa45f081bed64ef7190010c
hanqi <hanqi@ustcsoft.com>
2017-10-23 15:06:24 +0800

    Fix.

---
+++ Example/PHCyclePictureView/PHCyclePictureView.h
@@ -0,0 +1,19 @@
+//
+//  PHCyclePictureView.h
+//  PHCyclePictureView
+//
+//  Created by Master on 2017/10/23.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHCyclePictureView.
+FOUNDATION_EXPORT double PHCyclePictureViewVersionNumber;
+
+//! Project version string for PHCyclePictureView.
+FOUNDATION_EXPORT const unsigned char PHCyclePictureViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHCyclePictureView/PublicHeader.h>
+
+
---
+++ Example/PHCyclePictureViewFramework/PHCyclePictureViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  PHCyclePictureViewFramework.h
+//  PHCyclePictureViewFramework
+//
+//  Created by Master on 2017/10/23.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHCyclePictureViewFramework.
+FOUNDATION_EXPORT double PHCyclePictureViewFrameworkVersionNumber;
+
+//! Project version string for PHCyclePictureViewFramework.
+FOUNDATION_EXPORT const unsigned char PHCyclePictureViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHCyclePictureViewFramework/PublicHeader.h>
+
+
